c0bbd3152694a5eebe22e4f46667176498be46a5
Shariful Islam <si.nayon@gmail.com>
2020-05-17 21:55:29 +0600

    unnecessary component delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header";
-// import Footer from "./components/footer";
-// import Banner from "./components/Banner";
-// import ReviewSlider from "./components/ReviewSlider";
-// import ConferenceList from "./components/ConferenceList";
-// import PopularPlace from "./components/PopularPlace";
-// import FlipBanner from "./components/FlipBanner";
 function App() {
   return (
     <div>

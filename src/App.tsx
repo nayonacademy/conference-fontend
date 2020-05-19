987051b4a39927750ca7e52c8c41c7e091890054
Shariful Islam <si.nayon@gmail.com>
2020-05-19 22:02:37 +0600

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/header";
+import Footer from "./components/footer";
+import Conferences from "./components/conferenses";
 function App() {
   return (
     <div>
     <Header />
-
+    <Conferences />
+    <Footer />
     </div>
   );
 }

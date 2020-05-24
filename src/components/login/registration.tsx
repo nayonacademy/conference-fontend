ba0475eef1c0602805dffb8ccd78178f877e5de3
Shariful Islam <si.nayon@gmail.com>
2020-05-24 18:26:29 +0600

    user registration done

--- src/components/login/registration.tsx
+++ src/components/login/registration.tsx
@@ -6,45 +6,46 @@ import { NavLink } from 'react-router-dom'
 const API_URL = process.env.REACT_APP_API_URL;
 class Registration extends React.Component<any, any> {
 
-  state = {
-    email: '',
-    password:'',
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      email: '',
+      password:'',
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleChange(event:any) {
-    this.setState({ email: event.target[0].value, password:event.target[1].value});
+  handleChange = (event:any) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    this.setState({[name]:value});
   }
 
   handleSubmit(event:any) {
     event.preventDefault();
-
-    const user = {
-      email: "nayon@genex.digital",
-      password: "pass1234"
+    const users = {
+      email: this.state.email,
+      password: this.state.password
     };
+// POST data
+        axios
+        .post(
+          API_URL + '/user',
+          users,
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(res => {
+          console.log(res);
+          console.log(res.status);
+          if (res.status == 200){
+            window.location.href = "/dashboard";
+          }
+        });
 
-    // axios.post(`http://localhost:8080/user`, { user })
-    //   .then(res => {
-    //     console.log(res);
-    //     console.log(res.data);
-    //   })
   }
-
-  // constructor(props:any) {
-  //   super(props);
-
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
-
-  // handleChange(event:any) {
-  //   this.setState({value: event.target.value});
-  // }
-
-  // handleSubmit(event:any) {
-  //   alert('An essay was submitted: ' + this.state.value);
-  //   event.preventDefault();
-  // }
 
   render() {
     return (
@@ -53,35 +54,29 @@ class Registration extends React.Component<any, any> {
       <div className="access-container w-container">
         <div className="card sign-up">
           <h1 className="heading login">Login to read and write reviews for your favorite conferences. </h1>
-          <div className="proceed-with-email-container">
-            <a href="/registration" className="continue-with-email-instead">Continue with email instead</a>
-          </div>
           <div className="">
             <div className="">
-
 
               <form onSubmit = { this.handleSubmit }>
                 <div className="">
                     <label className="email-label">Email Address</label>
-                    <input type="email" className="user-email w-input" max={256} name="email" placeholder="Email address" />
+                    <input type="email" onChange={this.handleChange} className="user-email w-input" max={256} name="email" placeholder="Email address" />
                 </div>
                 <div className="">
                     {/* <label className="user-name-label">Name (public)</label> */}
                     {/* <input type="text" className="user-public-name w-input" max={256} name="name" placeholder="Name"/> */}
                     <label className="user-name-label">Password </label>
-                    <input type="text" className="user-public-name w-input" max={256} name="password" placeholder="Password"/>
-                    <label className="">
+                    <input type="text" onChange={this.handleChange} className="user-public-name w-input" max={256} name="password" placeholder="Password"/>
+                    {/* <label className="">
                     <input type="checkbox"name="agree" className="w-checkbox-input"/>
-                    {/* <span className="w-form-label">I agree to the <a href="#">terms</a> and <a href="#">conditions.</a></span> */}
-                    </label>
+                    <span className="w-form-label">I agree to the <a href="#">terms</a> and <a href="#">conditions.</a></span>
+                    </label> */}
                 </div>
                 <div>
-                  {/* <input type="submit" value="Sign In" className="w-button"/> */}
-                  <NavLink className="w-button" to="/dashboard"> Sign In </NavLink>
+                  <input type="submit" value="Sign Up" className="w-button"/>
+                  {/* <NavLink className="w-button" to="/dashboard"> Sign In </NavLink> */}
                 </div>
-                
               </form>
-
 
               <div className="w-form-done">
                 <div>Thank you! Your submission has been received!</div>
@@ -115,8 +110,6 @@ class Registration extends React.Component<any, any> {
       </div>
     </div>
     </>
-
-
     );
   }
 }
